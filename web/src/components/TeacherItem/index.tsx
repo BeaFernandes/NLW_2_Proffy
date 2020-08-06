09642e4cd39de296fb5622ff5b1336acd92e2236
BeaFernandes <beatrizalmeida354@gmail.com>
2020-08-05 21:19:47 -0300

    Progress NLW day one

---
+++ web/src/components/TeacherItem/index.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+
+import './styles.css';
+
+function TeacherItem() {
+    return(
+        <article className="teacher-item">
+            <header>
+                <img src="https://avatars0.githubusercontent.com/u/43864137?s=400&u=0d226d5edb35f53742ec9b39f96be6f88f10fca7&v=4" alt="Beatriz Fernandes"/>
+                <div>
+                    <strong>Beatriz Fernandes</strong>
+                    <span>Química</span>
+                </div>
+            </header>
+            <p>Entusiasta das melhores tecnologias de química avançada.
+                <br/><br/>
+                Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.
+            </p>
+
+            <footer>
+                <p>
+                    Preço/hora
+                    <strong>R$ 80,00</strong>
+                </p>
+                <button type="button">
+                    <img src={whatsappIcon} alt="Whatsapp"/>
+                    Entrar em contato
+                </button>
+            </footer>
+        </article>
+    );
+}
+
+export default TeacherItem;
